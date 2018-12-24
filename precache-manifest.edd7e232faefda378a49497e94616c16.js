7901697f86f53b989ce9da0bdd4a2d568e3b60d5
Dragos Tudorescu <d.tudorescu@plan-net.com>
2018-12-24 20:56:47 +0200

    Updates

---
+++ precache-manifest.edd7e232faefda378a49497e94616c16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "4c4156cb5b7b2c7a46dd",
+    "url": "/contactmanager/static/js/main.4c4156cb.chunk.js"
+  },
+  {
+    "revision": "9be151f5646b6d2753dc",
+    "url": "/contactmanager/static/js/1.9be151f5.chunk.js"
+  },
+  {
+    "revision": "4c4156cb5b7b2c7a46dd",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "9be151f5646b6d2753dc",
+    "url": "/contactmanager/static/css/1.827c0e70.chunk.css"
+  },
+  {
+    "revision": "88636de754aedf8032fe8e0f089698ff",
+    "url": "/contactmanager/index.html"
+  }
+];
